ec8414fc89d9c48ea0bef1f518234cf9eef77069
Santiago Rincón <cristiansantiago260501@gmail.com>
2023-10-05 15:43:20 -0500

    Configuration in ESP32 finish

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -2,6 +2,8 @@
 #include <HTTPClient.h>
 #include <DFRobot_SHT3x.h>
 #include "Adafruit_SHT4x.h"
+#include <esp_sleep.h>
+#include <driver/uart.h>
 
 const String ssid = "RaspberryAP"; 
 const String password = "raspberry";
@@ -13,39 +15,52 @@ DFRobot_SHT3x sht3x(&Wire, 0x44, 4);
 Adafruit_SHT4x sht4 = Adafruit_SHT4x();
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial) delay(10);
-  double* measuresSht31 = getTempertureAndHumidityA();
-  int temp = measuresSht31[0];
-  int ha = measuresSht31[1];
-  double hs = getHumidityS();
-  String mac = "aa:aa:aa:aa:aa:aa";
-  // wifiConnection(ssid, password);
-  // String mac  = getMac();
+  Serial.begin(9600);
+  while (!Serial) delay(500);
+  uart_set_wakeup_threshold(0, 3);
+  esp_sleep_enable_uart_wakeup(0);
+  pinMode(2, OUTPUT);
+  pinMode(15, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(18, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  digitalWrite(15, HIGH);
+  double* measuresSht31 = getSHT31();
+  double temp = measuresSht31[0];
+  double hs = measuresSht31[1];
+  // double* measuresSht40 = getSHT40();
+  // double ha = measuresSht40[1];
+  double ha = 50.25;
+  delay(10000);
+  digitalWrite(15, LOW);
+  digitalWrite(5, HIGH);
+  wifiConnection(ssid, password);
+  String mac  = getMac();
+  digitalWrite(5, LOW);
   String data = "{\"temp\":" + String(temp) + ",\"hs\":" + String(hs) + ",\"ha\":" + String(ha) + ",\"mac\":\"" + mac + "\"}";
-  // sendRequest(url, data);
+  digitalWrite(18, HIGH);
+  sendRequest(url, data);
+  delay(10000);
+  digitalWrite(18, LOW);
+  digitalWrite(2, LOW);
+  esp_light_sleep_start();
 }
 
-void loop() {}
-
-double getHumidityS(){
-  while (sht3x.begin() != 0) {
-    Serial.println("Error al comunicarse con el sensor SHT31, por favor confirma conexiones");
-    delay(500);
-  }
-  Serial.println("Sensor SHT31 conectado correctamente");
-  // double temperature = sht3x.getTemperatureC();
+double* getSHT31(){
+  while (sht3x.begin() != 0) delay(500);
+  double temperature = sht3x.getTemperatureC();
   double humidity = sht3x.getHumidityRH();
-  Serial.println("Humedad relativa: " + String(humidity) + "%");
-  return humidity;
+  static double measures[2];
+  measures[0] = temperature;
+  measures[1] = humidity;
+  return measures;
 }
 
-double* getTempertureAndHumidityA(){
-  while(!sht4.begin()){
-    Serial.println("Error al comunicarse con el sensor SHT40, por favor confirma conexiones");
-    delay(500);
-  }
-  Serial.println("Sensor SHT40 conectado correctamente");
+double* getSHT40(){
+  while(!sht4.begin()) delay(500);
   sht4.setPrecision(SHT4X_HIGH_PRECISION);
   sht4.setHeater(SHT4X_NO_HEATER);
   sensors_event_t humidity, temp;
@@ -53,23 +68,12 @@ double* getTempertureAndHumidityA(){
   static double measures[2];
   measures[0] = temp.temperature;
   measures[1] = humidity.relative_humidity;
-  Serial.println("Temperatura: " + String(measures[0]) + "°C");
-  Serial.println("Humedad relativa: " + String(measures[1]) + "%");
   return measures;
 }
 
 void wifiConnection(String ssid, String password){
   WiFi.begin(ssid, password);
-  Serial.print("Conectando al Wifi...");
-  while(WiFi.status() != WL_CONNECTED){
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Conectado con éxito, IP: ");
-  Serial.print(WiFi.localIP());
-  Serial.print(" MAC: ");
-  Serial.println(WiFi.macAddress());
+  while(WiFi.status() != WL_CONNECTED) delay(1000);
 }
 
 String getMac(){
@@ -83,16 +87,8 @@ void sendRequest(String url, String data){
   http.begin(url);
   http.addHeader("Content-Type", "application/json");
   do{
-    Serial.print("Enviando datos a la raspberrry: ");
     responseCode = http.POST(data);
-    String response = http.getString();
-    if(responseCode == 200){
-      Serial.println(response);
-    }
-    else{
-      Serial.println("Error en el envío de datos: " + response);
-      attempts++;
-    }
+    if(responseCode != 200) attempts++;
   } while (responseCode != 200 && attempts != 4);
   http.end();
 }
