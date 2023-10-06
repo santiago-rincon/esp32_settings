82d7485d79c14ef2a04f6962ddd21b393b9ad9c5
Santiago Rincón <cristiansantiago260501@gmail.com>
2023-10-06 14:18:27 -0500

    Update README and add comments in all code

--- README.md
+++ README.md
@@ -1,21 +1,72 @@
+# Checklist
+- [x] Conexión con la estación base
+- [x] Identificación del nodo a través de la MAC en la estación base
+- [x] Mediciones de los sensores
+- [x] Envío de datos a la estación base
+- [x] Modo light sleep controlado por la estación base.
 # Configuración del IDE
 1. Descargar del gestor de placas de Arduino IDE `esp32`.
-2. Instalar las siguientes librerías (disponibles en la carpeta `libs` de este repositorio)
+	![A](assets/libreria_esp.png)
+2. Instalar las siguientes librerías (disponibles en la carpeta `libs` de este repositorio o en los enlaces adjuntos)
 	- [Librería para el sensor SHT31](https://github.com/DFRobot/DFRobot_SHT3x) 
 	- [Librería para el sensor SHT40](https://github.com/adafruit/Adafruit_SHT4X)
 	- [Librería Sensor driver (necesaria para el funcionamiento de la librería SHT40)](https://github.com/adafruit/Adafruit_Sensor)
 	- [Librería BusIO (necesaria para el funcionamiento de la librería SHT40)](https://github.com/adafruit/Adafruit_BusIO)
+	Una vez descargadas todas las librerías instalarlas de la siguiente manera (se debe realizar el proceso con cada una):
+	![A](assets/librerias.png)
+	![A](assets/librerias2.png)
 # Instalación del driver de la ESP32 (CP210X) en caso de requerirlo
-El driver esta disponible en la carpeta `drivers` o en la [página oficial](https://www.silabs.com/developers/usb-to-uart-bridge-vcp-drivers?tab=downloads)
-# Conexiones de los sensores
-Pines de la ESP32:
-- **SDA**: GPIO D21
-- **SCL**: GPIO D22
-
-Sensor SHT31
+El driver esta disponible en la carpeta `driver` o en la [página oficial](https://www.silabs.com/developers/usb-to-uart-bridge-vcp-drivers?tab=downloads). El driver se debe instalar en caso de que ele quipo no reconozca la ESP32 una vez realizado el paso 1. Puede seguir el siguiente [tutorial](https://www.intel.la/content/www/xl/es/support/articles/000033005/intel-nuc.html) para la instalación.
+# Conexiones del módulo HC12 y de los sensores SHT31 y SHT40
+La conexión de los sensores y el módulo se presenta en las siguientes imágenes, las resistencias en los pines *sda* y *sck* pueden tomar cualquier valor entre 1k$\Omega$ y 10k$\Omega$. Estas cumplen la función de garantizar un estado lógico alto mientras no hay transmisión de datos.  
 
 ![Conexión del sensor SHT31](assets/sht31.png)
 
-Sensor SHT40
+![Conexión del sensor SHT40](assets/sht40.png)
+***NOTA:*** La conexión mostrada anteriormente en los pines de la ESP32 es ilustrativa, verificar cada pin en la siguiente imagen. 
 
-![Conexión del sensor SHT31](assets/sht40.png)
+![pinout](assets/pinout.png)
+
+- **Pin SCL o SCK:** GPIO 22
+- **Pin SDA:** GPIO 21
+- **Pin TX0:** GPIO 1
+- **Pin RX0:** GPIO 3
+# Consideraciones del código
+## Conexión de los sensores SHT
+Debido a que ambos sensores presentan la misma dirección (0x44) por defecto, y no se puede cambiar fácilmente, solo se puede conectar un sensor a la vez. A continuación se describe que líneas comentar y descomentar en caso de conectar uno u otro sensor.
+- **SHT31**: con este sensor conectado es necesario descomentar las líneas 36, 37, 38 y 43 y comentar las líneas 39, 40, 41 y 42.
+- **SHT40**: con este sensor conectado es necesario descomentar las líneas 39, 40, 41 y 42 y comentar las líneas 36, 37, 38 y 43.
+## Conexión de futuros sensores
+En caso de que posteriormente se desea conectar sensores que midan dióxido de carbono o luminosidad se debe editar las funciones de `getCo2()` (líneas112-114) y `getLuminosity()` (líneas 117-119) respectivamente.
+***IMPORTANTE:*** al agregar código a estas funciones es importante que el valor retornado sea un número tipo `double` o `int` (en caso de ser `int` editar el principio de la función), no puede ser un `string`, un `array`, un `byte` o cualquier otro tipo de dato.
+## Línea 52. Construcción de los datos en formato JSON
+En esta línea se crea la data que será enviada a través del protocolo HTTP, por defecto se están creando todos los campos posibles, pero el único campo obligatorio para que la estación base responda con un código de estado exitoso es el de `mac` (la cual debe estar registrada en la base de datos de la estación base, ya sea por la interfaz local o por la página web), por ende, si no se desea mandar alguno de los otros datos, se pueden eliminar. Además, el nombre de los campos no puede ser cambiado, debe seguir el siguiente "contrato".
+```typescript
+{
+	"mac": "string",
+	"temp": number,
+	"ha": number,
+	"hs": number,
+	"rad": number,
+	"co2": number,
+}
+```
+A continuación se presentan algunas variantes:
+- Envío de `temperatura` 
+	```c
+	  String data = "{\"temp\":" + String(temp) + ",\"mac\":\"" + mac + "\"}";
+	```
+- Envío de `temperatura`  y `humedad ambiente`
+	```c
+	  String data = "{\"temp\":" + String(temp) + ",\"ha\":" + String(ha) + ",\"mac\":\"" + mac + "\"}";
+	```
+- Envío de `temperatura` ,  `humedad ambiente` y  `humedad del suelo`
+	```c
+	  String data = "{\"temp\":" + String(temp) + ",\"ha\":" + String(ha) + ",\"hs\":" + String(hs) + ",\"mac\":\"" + mac + "\"}";
+	```
+# Indicadores led
+Las resistencias conectada al cátodo de cada led es únicamente de protección, esta puede rondar entre los 220$\Omega$ y los 440$\Omega$. 
+- **Led verde:** este led estará encendido mientras la ESP32 realiza el procedimiento de lectura de los diferentes  sensores.
+- **Led amarillo:** este led estará encendido mientras la ESP32 realiza el procedimiento de conexión al punto de acceso WiFi de la estación base.
+- **Led rojo:** este led estará encendido mientras la ESP32 realiza el procedimiento de envío de datos a través del protocolo HTTP a la estación base.
+- **Led integrado de la ESP32 (pin 2)**: este led estará encendido mientras la ESP32 esta despierta y realiza todo el proceso de lectura, conexión y envío de datos, y se apagará una vez la ESP32 entre en modo `light sleep`.
--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,94 +1,119 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <DFRobot_SHT3x.h>
-#include "Adafruit_SHT4x.h"
-#include <esp_sleep.h>
-#include <driver/uart.h>
+/* Líbrerias necesarias para el correcto funcionamiento de todo el sistema */
+#include <WiFi.h> // Líbreria para la conexión WiFi
+#include <HTTPClient.h> // Líbreria para las peticiones HTTP a la estación base
+#include <DFRobot_SHT3x.h> // Líbreria para la serie de sensores SHT3X (SHT31)
+#include "Adafruit_SHT4x.h" // Líbreria para la serie de sensores SHT4X (SHT40)
+#include <esp_sleep.h>  // Líbreria para el manejo de los modos de energía
+#include <driver/uart.h>  // Líbreria para controlar despertar  la ESP de modo Light sleep a través del UART 0
 
-const String ssid = "RaspberryAP"; 
-const String password = "raspberry";
-const String host = "192.168.20.1";
-const String port = "8080";
-const String url = "http://" + host + ":" + port;
+/* Configuración de constantes para la comunicación con la estación base */
+const String ssid = "RaspberryAP";  // Nombre del SSID del punto de acceso creado por la estación base
+const String password = "raspberry";  // Contraseña para conectarse al punto de acceso creado por la estación base
+const String host = "192.168.20.1";   // Dirección IP del punto de acceso creado por la estación base (Gateway)
+const String port = "8080";   // Número del puerto por el cual corre el servicio HTTP creado por la estación base
+const String url = "http://" + host + ":" + port;   // Concatenación de la url completa a partir de los anteriores datos
 
-DFRobot_SHT3x sht3x(&Wire, 0x44, 4);
-Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+/* Creación de objetos para la lectura de los sensores */
+DFRobot_SHT3x sht3x(&Wire, 0x44, 4);  // Creación de un objeto tipo SHT3X para la lectura del sensor SHT31 con dirección 0x44 (dirección por defecto)
+Adafruit_SHT4x sht4 = Adafruit_SHT4x();  // Creación de un objeto tipo SHT4X para la lectura del sensor SHT40 (la inicialización del objeto se encarga de buscar la dirección del dispositivo)
 
+/* Definición de la configuración inicial de puerto UART y otros pines para debuging */
 void setup() {
-  Serial.begin(9600);
-  while (!Serial) delay(500);
-  uart_set_wakeup_threshold(0, 3);
-  esp_sleep_enable_uart_wakeup(0);
-  pinMode(2, OUTPUT);
-  pinMode(15, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(18, OUTPUT);
+  Serial.begin(9600);   // Inicio del serial (UART0) a 9600 baudios (velocidad de comunicación por defecto de los módulos HC12)
+  while (!Serial) delay(500);   // Bucle infinito hasta que el Serial se inicie correctamente
+  uart_set_wakeup_threshold(0, 3);  // Establecimiento de umbral (3) para despertar la ESP32 a través del UART0 (0)
+  esp_sleep_enable_uart_wakeup(0);  // Establecimiento del puerto UART el cual va a despertar la ESP32 (el UART 2 no permite esta función)
+  pinMode(2, OUTPUT);   // Configuración del pin 2 como salida para conocer cuando la ESP32 esta en proceso de lectura, conexión y envío de datos (Proceso completo)
+  pinMode(15, OUTPUT);   // Configuración del pin 15 como salida para conocer cuando la ESP32 esta en proceso de lectura del los sensores
+  pinMode(5, OUTPUT);   // Configuración del pin 5 como salida para conocer cuando la ESP32 esta en proceso de conexión con el WiFi
+  pinMode(18, OUTPUT);   // Configuración del pin 18 como salida para conocer cuando la ESP32 esta en proceso de envío de datos a través del protocolo HTTP
 }
 
+/* Loop infinito que se ejecutará cada vez que la ESP32 despierte del modo Light sleep */
 void loop() {
-  digitalWrite(2, HIGH);
-  digitalWrite(15, HIGH);
-  double* measuresSht31 = getSHT31();
-  double temp = measuresSht31[0];
-  double hs = measuresSht31[1];
-  // double* measuresSht40 = getSHT40();
-  // double ha = measuresSht40[1];
-  double ha = 50.25;
-  delay(10000);
-  digitalWrite(15, LOW);
-  digitalWrite(5, HIGH);
-  wifiConnection(ssid, password);
-  String mac  = getMac();
-  digitalWrite(5, LOW);
-  String data = "{\"temp\":" + String(temp) + ",\"hs\":" + String(hs) + ",\"ha\":" + String(ha) + ",\"mac\":\"" + mac + "\"}";
-  digitalWrite(18, HIGH);
-  sendRequest(url, data);
-  delay(10000);
-  digitalWrite(18, LOW);
-  digitalWrite(2, LOW);
-  esp_light_sleep_start();
+  digitalWrite(2, HIGH);  // pin 2 encendido para notificar el inicio del proceso completo
+  digitalWrite(15, HIGH);  // pin 15 encendido para notificar el inicio del proceso de la lectura de sensores
+  double* measuresSht31 = getSHT31();   // Obtención de la temperatura la humedad relativa (vector de dos posisciones) del sensor SHT31
+  double temp = measuresSht31[0];   // Asiganación de la primera posición del vector obtenido a la variable temp
+  double hs = measuresSht31[1];   // Asiganación de la segunda posición del vector obtenido a la variable hs
+  // double* measuresSht40 = getSHT40();  // Obtención de la temperatura la humedad relativa (vector de dos posisciones) del sensor SHT40
+  // double temp = measuresSht40[0];   // Asiganación de la primera posición del vector obtenido a la variable temp
+  // double ha = measuresSht40[1];  // Asiganación de la segunda posición del vector obtenido a la variable ha
+  // double hs = 50.25;  // Valor asignado mientras se define el sensor de esta lectura
+  double ha = 50.25;  // Valor asignado mientras se define el sensor de esta lectura
+  double co2 = getCo2();  // Asignación de la cantidad de CO2 a través de la función getCo2
+  double rad = getLuminosity();  // Asignación de la cantidad de radiación solar a través de la función getLuminosity
+  delay(10000);   // Espera de 10 segundos para que le módulo WiFi se configure correctamente una vez despierta del modo Light sleep
+  digitalWrite(15, LOW);  // pin 15 apagado para notificar el fin del proceso de la lectura de sensores
+  digitalWrite(5, HIGH);  // pin 5 encendido para notificar el inicio del proceso de la conexion WiFi
+  wifiConnection(ssid, password);   // Intento de conexión al punto de acceso definido en las contantes globales
+  String mac  = getMac();   // Obtención de la dirección MAC una vez realizada la conexión exitosamente
+  digitalWrite(5, LOW);  // pin 5 apagado para notificar el fin del proceso de la conexion WiFi
+  String data = "{\"temp\":" + String(temp) + ",\"hs\":" + String(hs) + ",\"ha\":" + String(ha) + ",\"co2\":" + String(co2) + ",\"rad\":" + String(rad) + ",\"mac\":\"" + mac + "\"}";  // Construcción del objeto JSOn que se enviará a la estación base
+  digitalWrite(18, HIGH);  // pin 18 encendido para notificar el inicio del proceso de envío de datos a través del protocolo HTTP
+  sendRequest(url, data);   // Envío de datos a través del protocolo HTTP a la URL definida y con los datos construidos anteriormente
+  delay(10000);   // Espera de 10 segundos para asegurar el correcto envío de datos
+  digitalWrite(18, LOW);  // pin 18 apagado para notificar el fin del proceso de envío de datos a través del protocolo HTTP
+  digitalWrite(2, LOW);  // pin 2 apagado para notificar el fin del proceso completo
+  esp_light_sleep_start();  // ESP32 en modo light sleep
 }
 
-double* getSHT31(){
-  while (sht3x.begin() != 0) delay(500);
-  double temperature = sht3x.getTemperatureC();
-  double humidity = sht3x.getHumidityRH();
-  static double measures[2];
-  measures[0] = temperature;
-  measures[1] = humidity;
-  return measures;
-}
-
-double* getSHT40(){
-  while(!sht4.begin()) delay(500);
-  sht4.setPrecision(SHT4X_HIGH_PRECISION);
-  sht4.setHeater(SHT4X_NO_HEATER);
-  sensors_event_t humidity, temp;
-  sht4.getEvent(&humidity, &temp);
-  static double measures[2];
-  measures[0] = temp.temperature;
-  measures[1] = humidity.relative_humidity;
-  return measures;
-}
-
+/* Conexión del módulo WiFi (recibe por parámetros el ssid y la contraseña) */
 void wifiConnection(String ssid, String password){
-  WiFi.begin(ssid, password);
-  while(WiFi.status() != WL_CONNECTED) delay(1000);
+  WiFi.begin(ssid, password);   // Inicialización de un objeto WiFi con el ssid y la contraseña proporcionada por parámetros
+  while(WiFi.status() != WL_CONNECTED) delay(1000);   // Bucle infinito hasta no lograr una conexión exitosa
 }
 
+/* Obtención de la dirección MAC de la ESP32 */
 String getMac(){
-  return WiFi.macAddress();
+  return WiFi.macAddress();   // Retorno en formato string de la dirección MAC de la ESP32
 }
 
+/* Envío de petición HTTP a la estación base (recibe por parámetros la URL destino y los datos en formato JSON) */
 void sendRequest(String url, String data){
-  int attempts = 0;
-  int responseCode = 0;
-  HTTPClient http;
-  http.begin(url);
-  http.addHeader("Content-Type", "application/json");
+  int attempts = 0;   // Creación de variable entera para controlar el número de intentos de la petición en caso de que falle
+  int responseCode = 0;   // Creación de variable entera para controlar el código de estado de la petición HTTP
+  HTTPClient http;  // Creación de un objeto HTTP
+  http.begin(url);  // Inicialización del objeto HTTP con la URLproporcionada por parámetro
+  http.addHeader("Content-Type", "application/json");   // Adición de la cabecera para enviar los datos en formato JSON (necesaria para la estación base)
+  /* Inicio de un bucle controlado para envir la petici´´on HTTP más de unavez en caso de que falle la primera vez */
   do{
-    responseCode = http.POST(data);
-    if(responseCode != 200) attempts++;
-  } while (responseCode != 200 && attempts != 4);
-  http.end();
+    responseCode = http.POST(data);   // Almacenamiento del código de estado enviado por la estación base
+    if(responseCode != 200) attempts++;   // Condicional para aumentar el número de intentos en caso de que el código de estado sea diferente de 200 (200 OK)
+  } while (responseCode != 200 && attempts != 4);   // Control del bucle. El bucle se repite mientras el código de estado no sea 200 o en número de intentos sea difernete de 4 
+  http.end();   // Culminación del objeto HTTP para evitar desbordameinto de memoria
+}
+
+/* Obtención de la temperatura y humedad del sensor SHT31 */
+double* getSHT31(){
+  while (sht3x.begin() != 0) delay(500);   // Bucle infinito hasta que no se establezca comunicación con el sensor SHT31
+  double temperature = sht3x.getTemperatureC();   // Obtención de la temperatura en grados centigrados en formato flotente
+  double humidity = sht3x.getHumidityRH();   // Obtención de la humedad relativa (%) en formato flotente
+  static double measures[2];  // Creación de un vector de dos posiciones que se retornará posteriormente
+  measures[0] = temperature;  // Asignación de la temperatura leida a la primera posisción del vector
+  measures[1] = humidity;  // Asignación de la humedad relativa leida a la segunda posisción del vector
+  return measures;  // Retorno del vector con los valores de temperatura y humedad (primera y segunda posición respectivamente)
+}
+
+/* Obtención de la temperatura y humedad del sensor SHT31 */
+double* getSHT40(){
+  while(!sht4.begin()) delay(500);   // Bucle infinito hasta que no se establezca comunicación con el sensor SHT40
+  sht4.setPrecision(SHT4X_HIGH_PRECISION);  // Configuración de la presición del sensor
+  sht4.setHeater(SHT4X_NO_HEATER);  // Configuración del calentador interno del sensor
+  sensors_event_t humidity, temp;   // Creación de dos eventos que permiten leer la temperatura y humedad
+  sht4.getEvent(&humidity, &temp);  // Obtención de temperatura y humedad a través de los eventos creados anteriormente
+  static double measures[2];  // Creación de un vector de dos posiciones que se retornará posteriormente
+  measures[0] = temp.temperature;  // Asignación de la temperatura leida a la primera posisción del vector
+  measures[1] = humidity.relative_humidity;  // Asignación de la humedad relativa leida a la segunda posisción del vector
+  return measures;  // Retorno del vector con los valores de temperatura y humedad (primera y segunda posición respectivamente)
+}
+
+/* Incluir lógica necesaria dentro de la función para la lectura del sensor de CO2 */
+double getCo2(){
+  return 600.25;
+}
+
+/* Incluir lógica necesaria dentro de la función para la lectura del sensor de luminosidad o radiación solar */
+double getLuminosity(){
+  return 126.29;
 }
